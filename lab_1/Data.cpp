b22190ae3dbcd1b92c3581b9299c985760d97d97
Anastasiia Matviichuk <a.matviychuk0831@gmail.com>
2022-11-09 22:33:56 +0200

    Create a file with data

---
+++ lab_1/Data.cpp
@@ -0,0 +1,8 @@
+#include "Data.h"
+
+int Patient::Patient_ID = 0;
+int Doctor::Doctor_ID = 0;
+int Cabinet::Cabinet_ID = 0;
+int Card::Card_ID = 0;
+int Department::Department_ID = 0;
+int Service::Service_ID = 0;
