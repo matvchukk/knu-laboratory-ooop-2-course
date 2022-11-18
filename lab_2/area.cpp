e067030f73cdd4ebf9992eebca9519746b9e6d05
Anastasiia Matviichuk <a.matviychuk0831@gmail.com>
2022-11-18 17:56:19 +0200

    Added new functions and fixed bugs

--- lab_2/area.cpp
+++ lab_2/area.cpp
@@ -17,6 +17,92 @@ DrawingArea::DrawingArea(QWidget *parent) : QWidget(parent)
     myWidth = 2;
 }
 
+void DrawingArea::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        isDrawing = true;
+        lastPoint = event->pos();
+    }
+}
+
+void DrawingArea::mouseMoveEvent(QMouseEvent *event)
+{
+    if ((event->buttons() & Qt::LeftButton) && isDrawing)
+        drawLineTo(event->pos());
+}
+
+void DrawingArea::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton && isDrawing)
+    {
+        isDrawing = false;
+        drawLineTo(event->pos());
+    }
+}
+
+void DrawingArea::paintEvent(QPaintEvent *event)
+{
+    QPainter craft(this);
+    QRect updated = event->rect();
+    craft.drawImage(updated, image, updated);
+}
+
+void DrawingArea::resizeEvent(QResizeEvent *event)
+{
+    if (width() > image.width() || height() > image.height())
+    {
+        int newWidth = qMax(width() + 128, image.width());
+        int newHeight = qMax(height() + 128, image.height());
+        resizeImage(&image, QSize(newWidth, newHeight));
+        update();
+    }
+    QWidget::resizeEvent(event);
+}
+
+void DrawingArea::drawLineTo(const QPoint &endPoint)
+{
+    QPainter craft(&image);
+    craft.setPen(QPen(myColor, myWidth, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+    craft.drawLine(lastPoint, endPoint);
+    int newSize= (myWidth / 2) + 2;
+    update(QRect(lastPoint, endPoint).normalized().adjusted(-newSize, -newSize, +newSize, +newSize));
+    lastPoint = endPoint;
+
+    modified = true;
+}
+
+void DrawingArea::resizeImage(QImage *image, const QSize &newSize)
+{
+    if (image->size() == newSize)   return;
+
+    QImage picture(newSize, QImage::Format_RGB32);
+    picture.fill(qRgb(255, 255, 255));
+    QPainter craft(&picture);
+    craft.drawImage(QPoint(0, 0), *image);
+    *image = picture;
+}
+
+void DrawingArea::print()
+{
+    #if QT_CONFIG(printdialog)
+
+    QPrinter printer(QPrinter::HighResolution);
+    QPrintDialog printDialog(&printer, this);
+    if (printDialog.exec() == QDialog::Accepted)
+    {
+        QPainter painter(&printer);
+        QSize size = image.size();
+        QRect pos = painter.viewport();
+
+        size.scale(pos.size(), Qt::KeepAspectRatio);
+        painter.setViewport(pos.x(), pos.y(), size.width(), size.height());
+        painter.setWindow(image.rect());
+        painter.drawImage(0, 0, image);
+    }
+    #endif
+}
+
 void DrawingArea::setColor(const QColor &newColor)
 {
     myColor = newColor;
--- lab_2/mainwindow.cpp
+++ lab_2/mainwindow.cpp
@@ -12,8 +12,8 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(area);
     doAction();
     doMenu();
-    setWindowTitle(tr("This is your drawing area! :)"));
-    resize(500, 500);
+    setWindowTitle(tr("Hi, this is your PaintApp! :)"));
+    resize(1000, 500);
 }
 
 void MainWindow::closeEvent(QCloseEvent *event)
@@ -30,7 +30,14 @@ void MainWindow::closeEvent(QCloseEvent *event)
 
 void MainWindow::help()
 {
-    QMessageBox::about(this, tr("About App"), tr("<p>Some text hihi</p>"));
+    QMessageBox::about(this, tr("Help"), tr("<p> To start drawing, just hold down the left mouse button and draw a detour line. </p>"
+                                            "<p> Go to <b>Settings->Pen Color</b> to change the color of the brush </p>"
+                                            "<p> Go to <b>Settings->Pen Width</b> to change the width of the brush </p>"
+                                            "<p> Go to <b>Settings->Clear Screen</b> to clear the screen </p>"
+                                            "<p> Go to <b>File->Save AS</b> to save the picture in the selected format </p>"
+                                            "<p> Go to <b>File->Open</b> to open the drawing from the device </p>"
+                                            "<p> Go to <b>File->Print</b> to print the image </p>"
+                                            "<p> Go to <b>File->Exit</b> to close the application </p>"));
 }
 
 void MainWindow::color()
@@ -44,7 +51,7 @@ void MainWindow::color()
 void MainWindow::width()
 {
     bool set;
-    int newWidth = QInputDialog::getInt(this, tr("Scribble"),tr("Select pen width:"),area->penWidth(), 1, 50, 1, &set);
+    int newWidth = QInputDialog::getInt(this, tr("PaintApp"),tr("Select pen width:"),area->penWidth(), 1, 50, 1, &set);
     if (set)
         area->setWidth(newWidth);
 }
@@ -70,14 +77,14 @@ void MainWindow::doAction()
 {
     userActions = new QAction(tr("&Open..."), this);
     userActions->setShortcuts(QKeySequence::Open);
-    connect(userActions, SIGNAL(triggered()), this, SLOT(open()));
+    connect(userActions, SIGNAL(triggered()), this, SLOT(fileOpen()));
 
     foreach (QByteArray format, QImageWriter::supportedImageFormats())
     {
             QString text = tr("%1...").arg(QString(format).toUpper());
             QAction *action = new QAction(text, this);
             action->setData(format);
-            connect(action, SIGNAL(triggered()), this, SLOT(save()));
+            connect(action, SIGNAL(triggered()), this, SLOT(fileSave()));
             formatActions.append(action);
     }
 
@@ -85,21 +92,21 @@ void MainWindow::doAction()
     connect(printActions, SIGNAL(triggered()), area, SLOT(print()));
 
     colorActions = new QAction(tr("&Pen Color..."), this);
-    connect(colorActions, SIGNAL(triggered()), this, SLOT(penColor()));
+    connect(colorActions, SIGNAL(triggered()), this, SLOT(color()));
 
     widthActions = new QAction(tr("Pen &Width..."), this);
-    connect(widthActions, SIGNAL(triggered()), this, SLOT(penWidth()));
+    connect(widthActions, SIGNAL(triggered()), this, SLOT(width()));
 
     clearActions = new QAction(tr("&Clear Screen"), this);
     clearActions->setShortcut(tr("Ctrl+L"));
-    connect(clearActions, SIGNAL(triggered()), area, SLOT(clearImage()));
+    connect(clearActions, SIGNAL(triggered()), area, SLOT(clear()));
 
     exitActions = new QAction(tr("E&xit"), this);
     exitActions->setShortcuts(QKeySequence::Quit);
     connect(exitActions, SIGNAL(triggered()), this, SLOT(close()));
 
     helpActions = new QAction(tr("&Help"), this);
-    connect(helpActions, SIGNAL(triggered()), this, SLOT(about()));
+    connect(helpActions, SIGNAL(triggered()), this, SLOT(help()));
 
     QtHelpActions = new QAction(tr("About &Qt"), this);
     connect(QtHelpActions, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
@@ -118,7 +125,7 @@ void MainWindow::doMenu()
      menuFile->addAction(exitActions);
      menuFile->addSeparator();
 
-     menuOption = new QMenu(tr("&Options"), this);
+     menuOption = new QMenu(tr("&Settings"), this);
      menuOption->addAction(colorActions);
      menuOption->addAction(widthActions);
      menuOption->addAction(clearActions);
@@ -128,8 +135,8 @@ void MainWindow::doMenu()
      menuUserHelp->addAction(helpActions);
      menuUserHelp->addAction(QtHelpActions);
 
-     menuBar()->addMenu(menuFile);
      menuBar()->addMenu(menuOption);
+     menuBar()->addMenu(menuFile);
      menuBar()->addMenu(menuUserHelp);
 }
 
@@ -138,7 +145,7 @@ bool MainWindow::modificated()
     if (area->pictureModified())
     {
        QMessageBox::StandardButton askAbtSaving;
-       askAbtSaving = QMessageBox::warning(this, tr("Scribble"), tr("Do you want to save your changes?"), QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+       askAbtSaving = QMessageBox::warning(this, tr("PaintApp"), tr("Do you want to save your changes?"), QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
 
        if (askAbtSaving == QMessageBox::Save)
        {
